7a1c1a751fe025b9520f34428820f0722bdcbc9e
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-01-01 18:08:42 -0500

    Fix style issues.

--- game/ui/floating_window.py
+++ game/ui/floating_window.py
@@ -21,24 +21,25 @@ class FloatingWindow(DragBehavior, Widget):
 
         # Check if the touch was inside this widget
         if self.collide_point(touch.x, touch.y):
-            # Bring this widget to the top of the z-order by removing it from its parent and adding it back to
-            # its original parent.
+            # Bring this widget to the top of the z-order by removing it from
+            # its parent and adding it back to its original parent.
             parent = self.parent
 
             if parent is not None:
                 self.parent.remove_widget(self)
                 parent.add_widget(self)
-            
+
             # The event has been handled, so prevent further processing of it.
             return True
-        
+
         else:
             # Continue processing the event
             return False
-        
+
     def on_close(self):
-        # Remove this widget from its parent. If you need to reopen it later, be sure to keep a reference
-        # to prevent it from being garbage collected.
+        # Remove this widget from its parent. If you need to reopen it later,
+        # be sure to keep a reference to prevent it from being garbage
+        # collected.
         self.parent.remove_widget(self)
 
 
