9a046e1006fb3a6414504394cf10f61b9b717248
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-01-06 01:06:58 -0500

    Fix style issues.

--- game/ui/__init__.py
+++ game/ui/__init__.py
@@ -22,12 +22,12 @@ class NeoImpressiveTitleUI(App):
     def build(self):
         self.main_screen = MainScreen()
         return self.main_screen
-    
+
     def switch_to_screen(self, name):
         # Return if the main screen has not yet been created
         if self.main_screen is None:
             return
-        
+
         # Switch screens
         if name in ["TitleScreen"]:
             self.main_screen.transition = FadeTransition()
