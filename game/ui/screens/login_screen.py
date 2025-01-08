d6df3f35c0e77e7dfa5ba53c73d1e03786e3f75a
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-01-07 23:31:58 -0500

    Add credential validation to login screen.

--- game/ui/screens/login_screen.py
+++ game/ui/screens/login_screen.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.screenmanager import Screen
 
 import ui.theme
@@ -7,7 +8,13 @@ import ui.theme
 # Classes
 # =======
 class LoginScreen(Screen):
-    pass
+    login_btn = ObjectProperty(None)
+    username = StringProperty("")
+    password = StringProperty("")
+
+    def validate_credentials(self):
+        # Enable/disable login button based on the validity of the given credentials
+        self.login_btn.disabled = (len(self.username) < 1 or len(self.password) < 1)
 
 
 Builder.load_file("ui/screens/login_screen.kv")
