b75aa481c1f83acc70e39f55329d91ecacc9e4cb
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-01-16 17:06:06 -0500

    Replace option button with spinner.

--- game/ui/theme.py
+++ game/ui/theme.py
@@ -3,15 +3,16 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.dropdown import DropDown
 from kivy.uix.label import Label
 from kivy.uix.slider import Slider
+from kivy.uix.spinner import Spinner
 from kivy.uix.switch import Switch
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 
 from ui.floating_window import FloatingWindow
-from ui.option_button import OptionButton
 from ui.textured_progress import TexturedProgress
 
 # Initialize fonts
@@ -47,15 +48,6 @@ class GameLabel(Label):
     pass
 
 
-class GameOptionButton(OptionButton):
-    def __init__(self, *args, **kwargs):
-        # Call the base constructor
-        super().__init__(*args, **kwargs)
-
-        # Set the option class to our themed toggle button
-        # self.optionclass = GameToggleButton
-
-
 class GamePanel(BoxLayout):
     pass
 
@@ -72,6 +64,24 @@ class GamePopup(Label):
 
 
 class GameSlider(Slider):
+    pass
+
+
+class GameSpinner(Spinner):
+    def __init__(self, *args, **kwargs):
+        # Call the base constructor
+        super().__init__(*args, **kwargs)
+
+        # Set dropdown and option classes
+        self.dropdown_cls = GameSpinnerDropDown
+        self.option_cls = GameSpinnerOption
+
+
+class GameSpinnerDropDown(DropDown):
+    pass
+
+
+class GameSpinnerOption(ToggleButton):
     pass
 
 
