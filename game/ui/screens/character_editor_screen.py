eafc50bccc6ea30d52f1effd1b9229e6d8ca5099
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-01-16 20:14:53 -0500

    Setup auto body part detection.

--- game/ui/screens/character_editor_screen.py
+++ game/ui/screens/character_editor_screen.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from direct.stdpy.file import *
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.screenmanager import Screen
@@ -9,13 +12,37 @@ import ui.theme
 # =======
 class CharacterEditorScreen(Screen):
     character_name = StringProperty("")
-    body = ObjectProperty(None)
-    head = ObjectProperty(None)
-    mane = ObjectProperty(None)
-    tail = ObjectProperty(None)
-    wings = ObjectProperty(None)
-    tuft = ObjectProperty(None)
+    body_spinner = ObjectProperty(None)
+    head_spinner = ObjectProperty(None)
+    mane_spinner = ObjectProperty(None)
+    tail_spinner = ObjectProperty(None)
+    wing_spinner = ObjectProperty(None)
+    tuft_spinner = ObjectProperty(None)
     save_btn = ObjectProperty(None)
+
+    def on_body_spinner(self, instance, value):
+        # Populate body spinner
+        self.body_spinner.values = [Path(path).stem for path in listdir("meshes/player/bodies")]
+
+    def on_head_spinner(self, instance, value):
+        # Populate head spinner
+        self.head_spinner.values = [Path(path).stem for path in listdir("meshes/player/heads")]
+
+    def on_mane_spinner(self, instance, value):
+        # Populate mane spinner
+        self.mane_spinner.values = [Path(path).stem for path in listdir("meshes/player/manes")]
+
+    def on_tail_spinner(self, instance, value):
+        # Populate tail spinner
+        self.tail_spinner.values = [Path(path).stem for path in listdir("meshes/player/tails")]
+
+    def on_wing_spinner(self, instance, value):
+        # Populate wing spinner
+        self.wing_spinner.values = [Path(path).stem for path in listdir("meshes/player/wings")]
+
+    def on_tuft_spinner(self, instance, value):
+        # Populate tuft spinner
+        self.tuft_spinner.values = [Path(path).stem for path in listdir("meshes/player/tufts")]
 
     def validate_character_name(self):
         self.save_btn.disabled = len(self.character_name) == 0
