b204a70d58f757d6dac3f72a55b146427c34b388
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-05-30 23:35:43 -0400

    Refactor current background property of image button.

--- game/ui/image_button.py
+++ game/ui/image_button.py
@@ -1,6 +1,6 @@
 from kivy.lang import Builder
-from kivy.properties import StringProperty
-from kivy.uix.button import ButtonBehavior
+from kivy.properties import ListProperty, StringProperty
+from kivy.uix.button import Button, ButtonBehavior
 from kivy.uix.image import Image
 
 
@@ -11,22 +11,23 @@ class ImageButton(ButtonBehavior, Image):
     background_down = StringProperty()
     background_disabled_normal = StringProperty()
     background_disabled_down = StringProperty()
-    current_background = StringProperty()
+    border = ListProperty()
+    _current_background = StringProperty()
 
     def on_disabled(self, instance, value):
         if value:
-            self.current_background = self.background_disabled_normal
+            self._current_background = self.background_disabled_normal
 
         else:
-            self.current_background = self.background_normal
+            self._current_background = self.background_normal
 
     def on_state(self, instance, value):
         if not self.disabled:
             if value == "normal":
-                self.current_background = self.background_normal
+                self._current_background = self.background_normal
 
             elif value == "down":
-                self.current_background = self.background_down
+                self._current_background = self.background_down
 
 
 Builder.load_file("ui/image_button.kv")
