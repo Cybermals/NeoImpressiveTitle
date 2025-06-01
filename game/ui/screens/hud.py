087387d708298ebbbdbd0f9656d6d45bd50db53c
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-05-31 22:41:31 -0400

    Create home dialog.

--- game/ui/screens/hud.py
+++ game/ui/screens/hud.py
@@ -7,6 +7,8 @@ import ui.minimap  # noqa: F401
 import ui.player_hp_panel  # noqa: F401
 import ui.target_hp_panel  # noqa: F401
 
+from ui.dialogs.home_dialog import HomeDialog
+
 
 # Classes
 # =======
@@ -14,6 +16,35 @@ class HUD(Screen):
     chat_box = ObjectProperty()
     player_hp_panel = ObjectProperty()
     target_hp_panel = ObjectProperty()
+
+    def __init__(self, *args, **kwargs):
+        # Call base constructor
+        super().__init__(*args, **kwargs)
+
+        # Create dialogs
+        self.home_dlg = HomeDialog()
+
+    def toggle_home_dialog(self):
+        if self.home_dlg.parent is None:
+            self.add_widget(self.home_dlg)
+
+        else:
+            self.remove_widget(self.home_dlg)
+
+    def toggle_bio_dialog(self):
+        pass
+
+    def toggle_friend_dialog(self):
+        pass
+
+    def toggle_inventory_dialog(self):
+        pass
+
+    def toggle_actions_dialog(self):
+        pass
+
+    def toggle_party_dialog(self):
+        pass
 
 
 Builder.load_file("ui/screens/hud.kv")
