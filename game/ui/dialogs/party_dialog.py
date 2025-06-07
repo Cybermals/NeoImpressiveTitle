cccc840c75bfa57572f7495aaa25cc0729fa7ff0
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-06-07 18:25:05 -0400

    Create party dialog.

---
+++ game/ui/dialogs/party_dialog.py
@@ -0,0 +1,20 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+from ui.theme import GameFloatingWindow
+
+import ui.item_list
+
+
+# Classes
+# =======
+class PartyDialog(GameFloatingWindow):
+    party = ObjectProperty()
+    leave_party_btn = ObjectProperty()
+
+    def on_party(self, instance, value):
+        # Load test data
+        self.party.data = [{"text": f"Member {i + 1}"} for i in range(20)]
+
+
+Builder.load_file("ui/dialogs/party_dialog.kv")
