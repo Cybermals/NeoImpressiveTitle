308e771cb21c47d3543688969619b40be13baf5b
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-06-08 17:40:48 -0400

    Add inventory popup menu.

--- game/ui/item_list.py
+++ game/ui/item_list.py
@@ -1,9 +1,16 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.recycleview import RecycleView
+
+from ui.theme import GameButton
 
 
 # Classes
 # =======
+class ItemListEntry(GameButton):
+    command = ObjectProperty()
+
+
 class ItemList(RecycleView):
     pass
 
