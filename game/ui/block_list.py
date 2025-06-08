e50e382cb8ec036105e0252cd529d0a6704ff698
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-06-07 23:12:43 -0400

    Add block list popup menu.

--- game/ui/block_list.py
+++ game/ui/block_list.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.recycleview import RecycleView
 
 from ui.theme import GameButton
@@ -7,17 +8,11 @@ from ui.theme import GameButton
 # Classes
 # =======
 class BlockListEntry(GameButton):
-    def on_release(self):
-        print(f"Clicked blocked user {self.text}")
+    command = ObjectProperty()
 
 
 class BlockList(RecycleView):
-    def __init__(self, *args, **kwargs):
-        # Call base constructor
-        super().__init__(*args, **kwargs)
-
-        # Load sample block list
-        self.data = [{"text": f"Troublemaker {i + 1}"} for i in range(100)]
+    pass
 
 
 Builder.load_file("ui/block_list.kv")
--- game/ui/dialogs/friends_dialog.py
+++ game/ui/dialogs/friends_dialog.py
@@ -24,10 +24,27 @@ class FriendBubble(Bubble):
         # Close this widget
         self.parent.remove_widget(self)
         return False
+    
+
+class BlockedPlayerBubble(Bubble):
+    player = ObjectProperty()
+
+    def on_touch_up(self, touch):
+        # Call base method
+        super().on_touch_down(touch)
+
+        # Handled event?
+        if self.collide_point(touch.x, touch.y):
+            return True
+        
+        # Close this widget
+        self.parent.remove_widget(self)
+        return False
 
 
 class FriendsDialog(GameFloatingWindow):
     friends = ObjectProperty()
+    blocked_players = ObjectProperty()
 
     def on_touch_down(self, touch):
         self.mouse_pos = touch.pos
@@ -39,6 +56,13 @@ class FriendsDialog(GameFloatingWindow):
             "online": i % 4,
             "username": f"Friend {i + 1}",
             "command": lambda friend: self.show_friend_menu(friend)} for i in range(100)]
+        
+    def on_blocked_players(self, instance, value):
+        # Load sample block list
+        self.blocked_players.data = [{
+            "text": f"Troublemaker {i + 1}",
+            "command": lambda blocked_player: self.show_blocked_player_menu(blocked_player)
+            } for i in range(100)]
 
     def show_friend_menu(self, friend):
         # Create the friend bubble if it doesn't exist yet
@@ -54,6 +78,20 @@ class FriendsDialog(GameFloatingWindow):
         if self.friend_bubble.parent is None:
             self.add_widget(self.friend_bubble)
 
+    def show_blocked_player_menu(self, blocked_player):
+        # Create the blocked player bubble if it doesn't exist yet
+        if not hasattr(self, "blocked_player_bubble"):
+            self.blocked_player_bubble = BlockedPlayerBubble()
+
+        # Set selected blocked player and bubble position
+        self.blocked_player_bubble.player = blocked_player
+        self.blocked_player_bubble.center_x = self.mouse_pos[0]
+        self.blocked_player_bubble.y = self.mouse_pos[1]
+
+        # Show the widget if it isn't already visible
+        if self.blocked_player_bubble.parent is None:
+            self.add_widget(self.blocked_player_bubble)
+
     def message_friend(self, friend):
         print(f"Message '{friend.username}'")
         self.remove_widget(self.friend_bubble)
@@ -65,6 +103,10 @@ class FriendsDialog(GameFloatingWindow):
     def remove_friend(self, friend):
         print(f"Remove '{friend.username}'")
         self.remove_widget(self.friend_bubble)
+
+    def remove_blocked_player(self, blocked_player):
+        print(f"Unblock '{blocked_player.text}'")
+        self.remove_widget(self.blocked_player_bubble)
 
 
 Builder.load_file("ui/dialogs/friends_dialog.kv")
