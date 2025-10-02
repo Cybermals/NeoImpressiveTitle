42614a09950dd815f3af73f4f3bc2aaad4a5105a
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-10-01 21:10:44 -0400

    Reconfigure title screen to use IDs more effectively.

--- game/ui/screens/title_screen.py
+++ game/ui/screens/title_screen.py
@@ -1,6 +1,6 @@
 from direct.stdpy.file import open
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.properties import ListProperty
 from kivy.uix.screenmanager import Screen
 
 import ui.changelog  # noqa: F401
@@ -9,12 +9,15 @@ import ui.changelog  # noqa: F401
 # Classes
 # =======
 class TitleScreen(Screen):
-    changelog = ObjectProperty()
+    changelog = ListProperty()
 
-    def on_changelog(self, instance, value):
+    def __init__(self, *args, **kwargs):
+        # Call the base constructor
+        super().__init__(*args, **kwargs)
+
         # Load the changelog
         with open("changelog.txt") as f:
-            self.changelog.data = [{"text": " " if line == "\n" else line.rstrip()} for line in f]
+            self.changelog = [{"text": " " if line == "\n" else line.rstrip()} for line in f]
 
 
 Builder.load_file("ui/screens/title_screen.kv")
